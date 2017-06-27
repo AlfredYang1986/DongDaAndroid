441615dd1bf26bd92b33707aa053ddb86a659a52
AlfredYang1986 <yangyuanpig@gmail.com>
2017-06-27 11:19:19 +0800

    相册调用

---
+++ app/src/main/java/com/blackmirror/dongda/AY/AYIntentRequestCode.java
@@ -0,0 +1,8 @@
+package com.blackmirror.dongda.AY;
+
+/**
+ * Created by alfredyang on 02/06/2017.
+ */
+public class AYIntentRequestCode {
+    public static final int AY_INTENT_PICK_IMAGE_FROM_ALBUM = 100;
+}
--- app/src/main/java/com/blackmirror/dongda/Landing/PhotoChangeActivity.java
+++ app/src/main/java/com/blackmirror/dongda/Landing/PhotoChangeActivity.java
@@ -1,18 +1,30 @@
 package com.blackmirror.dongda.Landing;
 
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import com.blackmirror.dongda.AY.AYIntentRequestCode;
 import com.blackmirror.dongda.R;
 import com.blackmirror.dongda.controllers.AYActivity;
 import com.blackmirror.dongda.facade.DongdaCommonFacade.SQLiteProxy.DAO.AYDaoUserProfile;
+
+import java.io.IOException;
 
 public class PhotoChangeActivity extends AYActivity {
 
     final static String TAG = "Photo Change Activity";
 
     private AYDaoUserProfile p = null;
+
+    private final int REQUEST_CODE_PICK_IMAGE = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +40,50 @@ public class PhotoChangeActivity extends AYActivity {
                 Log.i(TAG, "enter button clicked");
             }
         });
+
+        ImageView iv = (ImageView) findViewById(R.id.landing_screen_photo_imgview);
+        iv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i(TAG, "start album");
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");//相片类型
+                startActivityForResult(intent, AYIntentRequestCode.AY_INTENT_PICK_IMAGE_FROM_ALBUM);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+
+        if (resultCode != RESULT_OK) {        //此处的 RESULT_OK 是系统自定义得一个常量
+            Log.e(TAG,"ActivityResult resultCode error");
+            return;
+        }
+
+        Bitmap bm = null;
+        //外界的程序访问ContentProvider所提供数据 可以通过ContentResolver接口
+        ContentResolver resolver = getContentResolver();
+        //此处的用于判断接收的Activity是不是你想要的那个
+        if (requestCode == REQUEST_CODE_PICK_IMAGE) {
+            try {
+                Uri originalUri = data.getData();        //获得图片的uri
+                bm = MediaStore.Images.Media.getBitmap(resolver, originalUri);        //显得到bitmap图片
+                // 这里开始的第二部分，获取图片的路径：
+                String[] proj = {MediaStore.Images.Media.DATA};
+                //好像是android多媒体数据库的封装接口，具体的看Android文档
+                Cursor cursor = managedQuery(originalUri, proj, null, null, null);
+                //按我个人理解 这个是获得用户选择的图片的索引值
+                int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                //将光标移至开头 ，这个很重要，不小心很容易引起越界
+                cursor.moveToFirst();
+                //最后根据索引值获取图片路径
+                String path = cursor.getString(column_index);
+
+            }catch (IOException e) {
+                Log.e(TAG, e.toString());
+            }
+        }
     }
 
     @Override
