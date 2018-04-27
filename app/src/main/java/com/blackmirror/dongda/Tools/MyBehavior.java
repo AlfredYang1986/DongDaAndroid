b718df2055c6c9a82486df13348af9397978eb56
xuchongxiang <xyjk1993@gmail.com>
2018-04-27 22:28:02 +0800

    fix

---
+++ app/src/main/java/com/blackmirror/dongda/Tools/MyBehavior.java
@@ -0,0 +1,26 @@
+package com.blackmirror.dongda.Tools;
+
+import android.content.Context;
+import android.support.design.widget.CoordinatorLayout;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by xcx on 18-4-25.
+ */
+public class MyBehavior extends CoordinatorLayout.Behavior<View> {
+
+    public MyBehavior(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean layoutDependsOn(CoordinatorLayout parent, View child, View dependency) {
+        return super.layoutDependsOn(parent, child, dependency);
+    }
+
+    @Override
+    public boolean onDependentViewChanged(CoordinatorLayout parent, View child, View dependency) {
+        return super.onDependentViewChanged(parent, child, dependency);
+    }
+}
